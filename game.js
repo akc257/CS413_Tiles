796f5f4b83b6679b0d2f6e3651a13a61e8ad254a
jscarter1227 <jscarter1227@gmail.com>
2019-10-28 21:04:31 -0700

    Added start scrren

--- game.js
+++ game.js
@@ -7,15 +7,50 @@ var gameport = document.getElementById("gameport");
 var renderer = new PIXI.autoDetectRenderer(GAME_WIDTH,
                                            GAME_HEIGHT,
                                            {backgroundColor: 0x99D5FF});
+
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
-stage.scale.x = GAME_SCALE;
-stage.scale.y = GAME_SCALE;
+// stage.scale.x = GAME_SCALE;
+// stage.scale.y = GAME_SCALE;
 
 // Scene objects get loaded in the ready function
 var playerUp, playerDown, playerRight, playerLeft;
 var world;
+var bool;
+
+//add start screen to scene graph
+var start_screen = new PIXI.Container();
+stage.addChild(start_screen);
+
+var start_screen_bg = new PIXI.Sprite(
+  PIXI.Texture.fromImage("title_screen.png") );
+
+//button to navigate menus
+var start_screen_button = new PIXI.Sprite(
+  PIXI.Texture.fromImage("play_button.png") );
+
+//start button to go to game
+start_screen_bg.addChild(start_screen_button);
+start_screen_button.position.x = 30;
+start_screen_button.position.y = 325;
+
+//add start screen background to start screen
+start_screen.addChild(start_screen_bg);
+
+//functon to switch screen when start button is clicked on
+function mouseStartHandler(e)
+{
+  stage.removeChild(start_screen);
+  stage.addChild(world);
+  stage.scale.x = GAME_SCALE;
+  stage.scale.y = GAME_SCALE;
+  bool = true;
+}
+
+//call mousehandler when screen clicked on
+start_screen_button.interactive = true;
+start_screen_button.on('mousedown', mouseStartHandler);
 
 function gameloop() {
         movePlayer();
@@ -88,7 +123,7 @@ PIXI.loader
 function ready() {
   var tu = new TileUtilities(PIXI);
   world = tu.makeTiledWorld("map_json", "map.png");
-  stage.addChild(world);
+  //stage.addChild(world);
 
   var frames = [];
   for( var i = 1; i <=3; i++)
@@ -110,6 +145,7 @@ function ready() {
 
   animate();
   update();
+  bool = false;
 }
 
 function animate(timestamp) {
@@ -123,8 +159,11 @@ function update(){
 }
 
 function update_camera() {
-  stage.x = -player.x*GAME_SCALE + GAME_WIDTH/2 - player.width/2*GAME_SCALE;
-  stage.y = -player.y*GAME_SCALE + GAME_HEIGHT/2 + player.height/2*GAME_SCALE;
-  stage.x = -Math.max(0, Math.min(world.worldWidth*GAME_SCALE - GAME_WIDTH, -stage.x));
-  stage.y = -Math.max(0, Math.min(world.worldHeight*GAME_SCALE - GAME_HEIGHT, -stage.y));
+  if( bool )
+  {
+    stage.x = -player.x*GAME_SCALE + GAME_WIDTH/2 - player.width/2*GAME_SCALE;
+    stage.y = -player.y*GAME_SCALE + GAME_HEIGHT/2 + player.height/2*GAME_SCALE;
+    stage.x = -Math.max(0, Math.min(world.worldWidth*GAME_SCALE - GAME_WIDTH, -stage.x));
+    stage.y = -Math.max(0, Math.min(world.worldHeight*GAME_SCALE - GAME_HEIGHT, -stage.y));
+  }
 }
